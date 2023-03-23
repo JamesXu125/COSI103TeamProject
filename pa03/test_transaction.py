cab11f1c82b039223d986a5ec5ee8a33d50cb32f
heathlh <110844948+heathlh@users.noreply.github.com>
2023-03-22 22:30:39 -0400

    Update test_transaction.py

--- pa03/test_transaction.py
+++ pa03/test_transaction.py
@@ -3,20 +3,34 @@ from transaction import Transaction
 
 @pytest.fixture
 def transaction():
-    return Transaction()
-  
+    # Create a temporary database file for testing
+    db_file = 'test.db'
+    # Create a Transaction instance using the temporary database file
+    t = Transaction(db_file)
+    # Yield the Transaction instance so it can be used in the tests
+    yield t
+    # Close the Transaction instance and delete the temporary database file after the tests
+    t.close()
+    os.remove(db_file)
+    
 def test_delete_transaction(transaction):
-    transaction.add_transaction(100, "food", "2023-03-20", "groceries")
+    # Call delete_transaction to delete the test transaction
+    transaction.add_transaction(1, 10.50, 'Groceries', '2022-03-22', 'Bought some apples and oranges')
     transaction.delete_transaction(1)
-    result = transaction.get_transactions()
-    assert len(result) == 0
+    # Use a SELECT statement to check if the transaction was deleted
+    transaction.cursor.execute("SELECT * FROM transactions WHERE id=?", (1,))
+    # Fetch the result of the SELECT statement
+    result = transaction.cursor.fetchone()
+    # Check that the result is None (i.e., the transaction was deleted)
+    assert result is None
 
-def test_get_transactions_by_date(transaction):
-    transaction.add_transaction(100, "food", "2023-03-20", "groceries")
-    transaction.add_transaction(200, "clothing", "2023-03-21", "new shirt")
-    result = transaction.get_transactions_by_date("2023-03-20")
-    assert len(result) == 1
-    assert result[0]["amount"] == 100
-    assert result[0]["description"] == "food"
-    assert result[0]["date"] == "2023-03-20"
-    assert result[0]["category"] == "groceries"
+def test_summarize_by_date(transaction):
+    # Add some test transactions to the database
+    transaction.add_transaction(1, 10.50, 'Groceries', '2022-03-22', 'Bought some apples and oranges')
+    transaction.add_transaction(2, 15.25, 'Gas', '2022-03-22', 'Filled up the tank')
+    transaction.add_transaction(3, 20.00, 'Dinner', '2022-03-23', 'Ate at a fancy restaurant')
+    # Call summarize_by_date to get a summary of transactions by date
+    summary = transaction.summarize_by_date()
+    # Check that the summary is correct
+    assert summary == [('2022-03-22', 25.75), ('2022-03-23', 20.0)]
+    
