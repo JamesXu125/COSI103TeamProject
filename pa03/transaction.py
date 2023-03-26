54f6dd3cbb9f7c15d024404605915fcd3449d3eb
heathlh <110844948+heathlh@users.noreply.github.com>
2023-03-25 23:20:03 -0400

    Update transaction.py

--- pa03/transaction.py
+++ pa03/transaction.py
@@ -1,7 +1,16 @@
 import sqlite3
 
 class Transaction:
+    """
+    A class to represent a transaction database.
+    """
     def __init__(self, db_path):
+        """
+        Constructs a Transaction object with a path to a SQLite database file.
+
+        Args:
+            db_path (str): The path to the SQLite database file.
+        """
         self.db_path = db_path
         self.runQuery('''CREATE TABLE IF NOT EXISTS transactions
                             (item_num INTEGER,
@@ -9,63 +18,99 @@ class Transaction:
                             category TEXT,
                             date TEXT,
                             description TEXT)''',())
-        # self.runQuery('''INSERT INTO transactions VALUES (1, 12345, 'test', '03/23/2023', 'run test 1')''',())
-        # self.runQuery('''INSERT INTO transactions VALUES (2, 3.14, 'test', '03/23/2023', 'run test 2')''',())
-        #self.runQuery('''INSERT INTO transactions VALUES (3, 37, 'not test', '07/21/2021', 'description')''',())
 
-    # get all transaction, method 4 
     def get_all_transactions(self):
+        """
+        Retrieves all transactions from the database.
+
+        Returns:
+            list: A list of dictionaries representing the transactions.
+        """
         return self.runQuery('''SELECT * FROM transactions''',())
-    def summarize_by_category(self):
-        return self.runQuery('''SELECT * FROM transactions GROUP BY category''',())
-    
-    # add one transaction, method 5 --Ziming
+
     def add_transaction(self, item):
+        """
+        Adds a transaction to the database.
+
+        Args:
+            item (dict): A dictionary representing the transaction.
+
+        Returns:
+            list: A list of dictionaries representing the transactions.
+        """
         return self.runQuery('''INSERT INTO transactions VALUES(?,?,?,?,?)''',(item['item_num'],item['amount'],
         item['category'],item['date'],item['description']))
-    
-    #delete the transaction by item number, method 6,  --hang liao
+
     def delete_transaction(self, item_num):
+        """
+        Deletes a transaction from the database by its item number.
+
+        Args:
+            item_num (int): The item number of the transaction to delete.
+        """
         return self.runQuery('''DELETE FROM transactions WHERE item_num=?''', (item_num,))
-    
-    #summarize the transaction by date, method 7,  --hangliao
+
     def summarize_transaction_by_date(self):
+        """
+        Summarizes the transactions in the database by date.
+
+        Returns:
+            list: A list of dictionaries representing the summarized transactions.
+        """
         conn = sqlite3.connect(self.db_path)
         cursor = conn.cursor()
         cursor.execute('SELECT date, SUM(amount) FROM transactions GROUP BY date')
         results = cursor.fetchall()
         return [{"date": date, "total_amount": amount} for date, amount in results]
-    
-    #summarize the transaction by month, method 8,  --zijun wang
+
     def summarize_transaction_by_month(self):
+        """
+        Summarizes the transactions in the database by month.
+
+        Returns:
+            list: A list of dictionaries representing the summarized transactions.
+        """
         conn = sqlite3.connect(self.db_path)
         cursor = conn.cursor()
         cursor.execute("SELECT substr(date,1,2) || '/' ||substr(date,7,10) AS month,SUM(amount) FROM transactions GROUP BY month")
         results = cursor.fetchall()
         return [{"date": date, "total_amount": amount} for date, amount in results]
-    
-    #summarize the transaction by year, method 9,  --zijun wang
+
     def summarize_transaction_by_year(self):
+        """
+        Summarizes the transactions in the database by year.
+
+        Returns:
+            list: A list of dictionaries representing the summarized transactions.
+        """
         conn = sqlite3.connect(self.db_path)
         cursor = conn.cursor()
         cursor.execute("SELECT substr(date,7,10) AS year, SUM(amount) FROM transactions GROUP BY year")
         results = cursor.fetchall()
         return [{"date": date, "total_amount": amount} for date, amount in results]
-    
+
+    def summarize_by_category(self):
+        """
+        Summarizes the transactions in the database by category.
+
+        Returns:
+            list: A list of dictionaries representing the summarized transactions.
+        """
+        return self.runQuery('''SELECT * FROM transactions GROUP BY category''',())
+
     def runQuery(self,query,tuple):
         ''' return all of the uncompleted tasks as a list of dicts.'''
         con = sqlite3.connect(self.db_path)
-        cur = con.cursor() 
+        cur = con.cursor()
         cur.execute(query,tuple)
         tuples = cur.fetchall()
         con.commit()
         con.close()
         return tuples_to_dicts(tuples)
 
-def to_dict(t):
-    transaction = {'item_num': t[0], 'amount': t[1], 'category': t[2], 'date': t[3], 'description': t[4]}
+def to_dict(tran):
+    transaction = {'item_num': tran[0], 'amount': tran[1], 'category': tran[2], 'date': tran[3], 'description': tran[4]}
     return transaction
 
-def tuples_to_dicts(ts):
-    return [to_dict(t) for t in ts]
-
+def tuples_to_dicts(trans):
+    return [to_dict(tran) for tran in trans]
